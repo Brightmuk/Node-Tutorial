905a358d83e38e96f7927c6670a47e39e2dae7e3
Brightmuk <be.rightmuk@gmail.com>
2019-10-02 12:55:04 +0300

    Add multiple records in table

--- Db/insertToTable.js
+++ Db/insertToTable.js
@@ -7,12 +7,53 @@ var con = mysql.createConnection({
   database: "mydb"
 });
 
+//For only a single record
+//------------------------
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("1 record inserted");
+//   });
+// });
+
 con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-  con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("1 record inserted");
+    console.log("Connected!");
+    var sql = "INSERT INTO customers (name, address) VALUES ?";
+var values = [
+    ['John', 'Highway 71'],
+    ['Peter', 'Lowstreet 4'],
+    ['Amy', 'Apple st 652'],
+    ['Hannah', 'Mountain 21'],
+    ['Michael', 'Valley 345'],
+    ['Sandy', 'Ocean blvd 2'],
+    ['Betty', 'Green Grass 1'],
+    ['Richard', 'Sky st 331'],
+    ['Susan', 'One way 98'],
+    ['Vicky', 'Yellow Garden 2'],
+    ['Ben', 'Park Lane 38'],
+    ['William', 'Central st 954'],
+    ['Chuck', 'Main Road 989'],
+    ['Viola', 'Sideway 1633']
+  ];
+  con.query(sql, [values], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+    //result object returned is in this form. Any prop can be dispalyed
+    //-----------------------------------------------------------------
+    // {
+    //     fieldCount: 0,
+    //     affectedRows: 14,
+    //     insertId: 0,
+    //     serverStatus: 2,
+    //     warningCount: 0,
+    //     message: '\'Records:14  Duplicated: 0  Warnings: 0',
+    //     protocol41: true,
+    //     changedRows: 0
+    //   }
   });
 });
